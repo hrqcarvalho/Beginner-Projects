f350ecf20477982c9511398425189443a979c4d5
Matheus Carvalho <matheus.bankai.bkp@gmail.com>
2023-04-02 14:40:26 -0300

    Adding the day 1 project: 'Band Name Generator'.

---
+++ band-name-generator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What is the name of the city you grew up in?\n")
+
+pets_name = input("What is your pet's name?\n")
+
+print(f"Your band name could be {city} {pets_name}.")
