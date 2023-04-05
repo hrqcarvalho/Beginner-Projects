38ca0f5a4c86a2410c3ce6a1a635bf8b6400f6fe
Matheus Carvalho <matheus.bankai.bkp@gmail.com>
2023-04-05 00:34:39 -0300

    README and Treasure Island update

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ The funny part is as you progress through the course the projects will become mo
     - A program that takes the city you grew up in and your pet's name and return a name for your band.
 - Tip Calculator
     - A program that takes a value, tip's percentage and a number of people and return the splitted bill.
+- Treasure Island
+    - A program that tells a short story (the point here is to test the knowledge about if-else statements).
--- treasure-island/main.py
+++ treasure-island/main.py
@@ -1,3 +1,8 @@
+"""
+A program that tells a short story (the point here is to test the knowledge about 
+if-else statements
+"""
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -26,4 +31,33 @@ print("Your mission is to find the treasure.\n")
 print("You are in a island looking for a treasure, feeling like Indiana Jones and then you\
  reach your first challenge:")
 print("You are on a narrow bridge and a huge round rock comes in your direction.")
-rock_challenge = input("You have to dodge it, which side do you choose? Left or Right?").lower()
+rock_challenge = input(
+    "You have to dodge it, which side do you choose? ['Left' or 'Right']? ").lower()
+
+if rock_challenge == 'left':
+    print("\nYou dodge it to the left, fall but hold on to a vine.")
+    print("Under you there is a river full of crocodiles.")
+
+    river_challenge = input(
+        "What do you do now? Keep holding or swim? ['Swim' or 'Hold']? ").lower()
+
+    if river_challenge == 'hold':
+        print("\nYou keep holding and then you see an entry of a cave in a mountain.")
+        print("You get all 'Tarzan' and manage to get there safely.")
+
+        door_challenge = input(
+            "Inside the cave you see three doors, which one do you open? ['Red', 'Yellow' or 'Blue']? ").lower()
+
+        if door_challenge == "yellow":
+            print("\nSomehow you did it! You found the Legendary Ancient Treasure!")
+        else:
+            print("\nYou open the door and there is an entire army of undead warriors.")
+            print("You are dead.")
+            print("Game Over.")
+    else:
+        print("\nYou push your luck and it fails, you are eaten.")
+        print("Game Over.")
+else:
+    print("\nYou dodge it to the right, fall and land right in front of a crocodile.")
+    print("You are pretty much dead.")
+    print("Game Over.")
