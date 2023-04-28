52b277c352d77fa4ee4698a9dd7a665baa73183b
Matheus Carvalho <matheus.bankai.bkp@gmail.com>
2023-04-27 23:38:18 -0300

    Final step - improving the player experience

--- hangman/main.py
+++ hangman/main.py
@@ -3,69 +3,18 @@ First of all, I apologize for this being such a messy, but i thought it would be
 the dr. made in order to guide us through this challenge.
 """
 from random import choice
+from hangman_art import *
+from hangman_words import word_list as wl
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["dragonborn", "kenway", "luffy"]
+# TODO-x: - Update the word list to use the 'word_list' from hangman_words.py
+# word_list = ["dragonborn", "kenway", "luffy"]
+word_list = wl
 
 #TODO-0: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
+#TODO-0.1: - Import the logo from hangman_art.py and print it at the start of the game.
 lives = 6
+print(logo)
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = choice(word_list)
@@ -82,8 +31,11 @@ print(f'Pssst, the solution is {chosen_word}.')
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 #TODO-2.1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters 
 # in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+#TODO-2.2: - If the user has entered a letter they've already guessed, print the letter and let them know.
 while '_' in display:
     guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
     #TODO-3.1: - Loop through each position in the chosen_word;
@@ -99,6 +51,8 @@ while '_' in display:
             if guess == each_letter:
                 display[index] = guess
     else:
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             print(stages[0])
